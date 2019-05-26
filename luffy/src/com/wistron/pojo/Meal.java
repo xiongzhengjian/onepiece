384c252f4aa72f4f206b8eb70a51290ce669a10e
xiongzhengjian <1913887509@qq.com>
2019-05-26 09:42:52 +0800

    big change

---
+++ luffy/src/com/wistron/pojo/Meal.java
@@ -0,0 +1,11 @@
+package com.wistron.pojo;
+
+import java.util.Date;
+
+public class Meal {
+	private int meal_id;
+	private Date date;
+	private String site;
+//	需要在user表中加入订餐状态这个字段，在meal表中减去status这个字段 
+	
+}
