03e3602e006ee07391f229b6c814a76436c66054
xiongzhengjian <1913887509@qq.com>
2019-06-18 20:47:15 +0800

    Corrected some minor problems

--- luffy/WebContent/doc/sqlscript/image_softroll_respin.sql
+++ luffy/WebContent/doc/sqlscript/image_softroll_respin.sql
@@ -16,19 +16,19 @@ alter table softroll_respin rename to softrollrespin;
 alter table softrollrespin add column owner varchar(15) after softrollrespin_id;
 
 
-describe softrollrespin;
+describe softrollrespin;	
 
 insert into softrollrespin 
 (softrollrespin_id,owner,chassis,platform,test_function,start,end,bios_version,image_build_id,test_plan,tester)
  values
-(7,'Tester1','NesslerP','Sunflower','FIT','2019-06-08 14:25:30','2019-06-11 14:25:30','F.20','18WW2SVT6AF','CDT-BIOS-Checklist_v4.6_pre-test','littlebear'),
-(8,'Tester2','NesslerP','Sunflower','WAT','2018-06-08 14:25:30','2019-06-11 11:40:30','F.20','18WW2SVT6AF','CDT-BIOS-Checklist_v4.6_pre-test','littlebear'),
-(9,'Tester3','NesslerP','Sunflower','SWT','2019-02-08 08:25:30','2019-06-11 11:40:30','F.20','18WW2SVT6AF','CDT-BIOS-Checklist_v4.6_pre-test','littlebear');
+(1,'Tester1','NesslerP','Sunflower','FIT','2019-06-08 14:25:30','2019-06-11 14:25:30','F.20','18WW2SVT6AF','CDT-BIOS-Checklist_v4.6_pre-test','littlebear'),
+(2,'Tester2','NesslerP','Sunflower','WAT','2018-06-08 14:25:30','2019-06-11 11:40:30','F.20','18WW2SVT6AF','CDT-BIOS-Checklist_v4.6_pre-test','littlebear'),
+(3,'Tester3','NesslerP','Sunflower','SWT','2019-02-08 08:25:30','2019-06-11 11:40:30','F.20','18WW2SVT6AF','CDT-BIOS-Checklist_v4.6_pre-test','littlebear');
 
 select * from softrollrespin;
 
 update softrollrespin set 			
-			owner='Tester1',
+			owner='me',
 		    chassis='NesslerP',
 		    platform='Sunflower',
 		    test_function='',		    	    
