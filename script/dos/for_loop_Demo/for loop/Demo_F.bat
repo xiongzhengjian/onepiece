08287305ac31b42de6b3c3efc03c6f61edac0f49
xiongzhengjian <1913887509@qq.com>
2020-10-27 11:29:32 -0700

    20201027

---
+++ script/dos/for_loop_Demo/for loop/Demo_F.bat
@@ -0,0 +1,21 @@
+﻿
+
+rem for 命令可以带参数或不带参数，带参数时支持/d /l /r /f
+
+goto Demo1
+:Demo1
+echo off
+for /f "usebackq tokens=1* delims==" %%i in (`set`) do (
+ @echo[%%i----%%j]
+)
+pause
+
+
+
+
+
+
+
+
+
+
